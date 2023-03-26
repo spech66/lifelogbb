8d0f89a6dd7639ff2a39d72d38ca52b5a975588b
Sebastian Pech <spech@spech.de>
2023-03-26 14:23:01 +0200

    Add sort indicators

---
+++ LifelogBb/Utilities/RazorPageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace LifelogBb.Utilities
+{
+    public static class RazorPageExtensions
+    {
+        public static string GetSortOrder<T>(ViewDataDictionary<T> viewData, string fieldName)
+        {
+            if (viewData["CurrentSort"] == null)
+            {
+                return "";
+            }
+
+            if ((string)viewData["CurrentSort"] == $"{fieldName}_desc")
+            {
+                return "desc";
+            }
+
+            else if ((string)viewData["CurrentSort"] == fieldName)
+            {
+                return "asc";
+            }
+
+            return "";
+        }
+    }
+}
