6a6b556b05cb1a81bca75c7f3a436866ba5849df
Sebastian Pech <spech@spech.de>
2023-04-10 12:16:56 +0200

    Weekly by day rules

--- LifelogBb/wwwroot/js/rruleeditor.js
+++ LifelogBb/wwwroot/js/rruleeditor.js
@@ -6,6 +6,7 @@ var reccurenceRulesModalTextElement = document.getElementById("rruleEditorModalT
 
 var rruleEditorModalFrequencySelect = document.getElementById("rruleEditorModalFrequency");
 var rruleEditorModalIntervalInput = document.getElementById("rruleEditorModalIntervalInput");
+var rruleEditorModalByDayInput = document.getElementsByName("rruleEditorModalByDayInput"); // Group!
 
 var rruleEditorModalEndSelect = document.getElementById("rruleEditorModalEnd");
 var rruleEditorModalUntilInput = document.getElementById("rruleEditorModalUntilInput");
@@ -13,11 +14,26 @@ var rruleEditorModalCountInput = document.getElementById("rruleEditorModalCountI
 
 
 function updateCurrentRule() {
+  // Stupid conversion because library doesn't support array of days/ints
+  var dayInputs = Array.prototype.slice.call(rruleEditorModalByDayInput, 0);
+  var weekdays = [];
+  dayInputs.forEach(s => {
+    if (!s.checked) return;
+    if (s.value === "MO") weekdays.push(rruleModule.RRule.MO);
+    else if (s.value === "TU") weekdays.push(rruleModule.RRule.TU);
+    else if (s.value === "WE") weekdays.push(rruleModule.RRule.WE);
+    else if (s.value === "TH") weekdays.push(rruleModule.RRule.TH);
+    else if (s.value === "FR") weekdays.push(rruleModule.RRule.FR);
+    else if (s.value === "SA") weekdays.push(rruleModule.RRule.SA);
+    else if (s.value === "SU") weekdays.push(rruleModule.RRule.SU);
+  });
+
   var rule = new rruleModule.RRule({
     freq: rruleEditorModalFrequencySelect.value,
     interval: rruleEditorModalFrequencySelect.value !== "0" ? rruleEditorModalIntervalInput.value : "",
     until: rruleEditorModalEndSelect.value === "on" && rruleEditorModalUntilInput.value != "" ? new Date(rruleEditorModalUntilInput.value) : null,
     count: rruleEditorModalEndSelect.value === "count" ? rruleEditorModalCountInput.value : null,
+    byweekday: weekdays,
   });
 
   console.log(rule.toString());
@@ -37,6 +53,7 @@ function updateFrequencyControls() {
   document.getElementById("rruleEditorModalIntervalInfo").innerText = newVal === "1" ? "Month(s)" : newVal === "2" ? "Week(s)" : "Day(s)";
 
   document.getElementById("rruleEditorModalIntervalContainer").hidden = newVal === "0"; // Hide on yearly
+  document.getElementById("rruleEditorModalByDayContainer").hidden = newVal !== "2"; // Show only on weekly
 }
 
 function updateEndControls() {
@@ -54,16 +71,22 @@ function updateEndControls() {
 
 document.addEventListener("DOMContentLoaded", function () {
   var currentRule = rruleInput.value === "" ? "FREQ=DAILY;INTERVAL=1" : rruleInput.value;
-  console.log(currentRule);
   const rule = rruleModule.RRule.fromString(currentRule);
-  console.log(rule.options);
 
   // Set initial values
   reccurenceRulesTextElement.innerText = rule.toText();
   reccurenceRulesModalTextElement.innerText = rule.toText();
   rruleEditorModalFrequencySelect.value = rule.options.freq;
+  rruleEditorModalIntervalInput.value = rule.options.interval;
   rruleEditorModalUntilInput.value = rule.options.until;
   rruleEditorModalCountInput.value = rule.options.count;
+
+  var days = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
+  if (rule.options.byweekday !== null) {
+    rruleEditorModalByDayInput.forEach(s => {
+      s.checked = rule.options.byweekday.includes(days.indexOf(s.value));
+    });
+  }
 
   updateFrequencyControls();
 
@@ -85,6 +108,12 @@ rruleEditorModalFrequencySelect.onchange = function () {
 rruleEditorModalIntervalInput.onchange = function () {
   updateCurrentRule();
 }
+
+rruleEditorModalByDayInput.forEach(s => {
+  s.onchange = function () {
+    updateCurrentRule();
+  }
+});
 
 rruleEditorModalEndSelect.onchange = function () {
   updateEndControls();
