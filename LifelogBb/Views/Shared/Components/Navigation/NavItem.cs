838bf535e5de0e94eefa8e7fec16b6408603f38f
Sebastian Pech <spech@spech.de>
2023-12-24 10:31:52 +0100

    Advanced navigation and active states

---
+++ LifelogBb/Views/Shared/Components/Navigation/NavItem.cs
@@ -0,0 +1,27 @@
+﻿namespace LifelogBb.Views.Shared.Components.Navigation
+{
+    public class NavItem
+    {
+        public string? Controller { get; set; }
+        public string? Action { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+
+        public List<NavItem> SubItems { get; set; } = new List<NavItem>();
+
+        public NavItem(string controller, string action, string title, string icon)
+        {
+            Controller = controller;
+            Action = action;
+            Title = title;
+            Icon = icon;
+        }
+
+        public NavItem(string title, string icon, List<NavItem> subItems)
+        {
+            Title = title;
+            Icon = icon;
+            SubItems = subItems;
+        }
+    }
+}
---
+++ LifelogBb/Views/Shared/Components/Navigation/Navigation.cs
@@ -0,0 +1,51 @@
+﻿using LifelogBb.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System;
+
+namespace LifelogBb.Views.Shared.Components.Navigation
+{
+    public class Navigation : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var list = new List<NavItem>
+            {
+                new NavItem("Home", "Index", "Home", "fas fa-home"),
+                new NavItem("Home", "Dashboard", "Dashboard", "fa-solid fa-chalkboard"),
+                new NavItem("Weight", "fas fa-weight-scale", new List<NavItem>
+                {
+                    new NavItem("Weights", "Index", "Weight", "fas fa-weight-scale"),
+                    new NavItem("Weights", "Graph", "Graph", "fas fa-chart-line"),
+                    // new NavItem("Weights", "Create", "Create", "fas fa-plus")
+                }),
+                new NavItem("Journals", "Index", "Journal", "fas fa-pencil"),
+                new NavItem("Strength training", "fas fa-dumbbell", new List<NavItem>
+                {
+                    new NavItem("StrengthTrainings", "Index", "Strength training", "fas fa-dumbbell"),
+                    new NavItem("StrengthTrainings", "Graph", "Graph", "fas fa-chart-line"),
+                }),
+                new NavItem("Endurance training", "fas fa-running", new List <NavItem>
+                {
+                    new NavItem("EnduranceTrainings", "Index", "Endurance training", "fas fa-running"),
+                    new NavItem("EnduranceTrainings", "Graph", "Graph", "fas fa-chart-line"),
+                }),
+                new NavItem("Todos", "Index", "Todos", "fas fa-check"),
+                new NavItem("Goals", "Index", "Goals", "fas fa-bullseye"),
+                new NavItem("Habits", "Index", "Habits", "fas fa-arrows-spin"),
+                new NavItem("Bucket list", "fas fa-tent", new List <NavItem>
+                {
+                    new NavItem("BucketLists", "Index", "Bucket list", "fas fa-tent"),
+                    new NavItem("BucketLists", "VisionBoard", "Vision board", "fas fa-chart-bar icon"),
+                }),
+                new NavItem("Quotes", "Index", "Quotes", "fas fa-quote-right")
+            };
+
+            return View(list);
+        }
+    }
+}
